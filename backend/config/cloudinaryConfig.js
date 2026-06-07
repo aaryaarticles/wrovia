7af98f60a4c453ca236ffec6fcabb1439a5423f2
aaryaarticles <aaryaarticles@gmail.com>
2026-06-07 22:32:29 +0530

    Add files via upload

---
+++ backend/config/cloudinaryConfig.js
@@ -0,0 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadProvider = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) cb(null, true);
+    else cb(new Error("Sirf images allowed hain!"), false);
+  },
+});
+
+module.exports = { cloudinary, uploadProvider };
